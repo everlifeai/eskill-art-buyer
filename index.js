3b9a2ec3d24bd8b85ca7378e11667f56a7cea0d9
Rajeshkannan <rrajeshkannanbca@gmail.com>
2021-05-03 12:23:32 +0530

    feat: initial version

--- index.js
+++ index.js
@@ -14,27 +14,45 @@ const https = require('https')
  * 
  *   outcome/
  * Start our microservice and register with the communication manager
- * and SSB
+ * and direct message
  */
 function main() {
   startMicroService()
   registerWithCommMgr()
   registerWithDirectMsg()
+  getWalletAccount()
 }
 
 let msKey = 'eskill-art-buyer-svc'
+
+const ARTSIE_BOT = '@qiqqqKggAr1Mix06/gP2PT1X7TtBGajJ2w3iZIjTvsc=.ed25519'
 
 const directMsgClient = new cote.Requester({
   name: 'art buyer ->  direct msg',
   key: 'everlife-dir-msg-demo-svc',
 })
 
+
+
 const stellarClient = new cote.Requester({
   name: 'art buyer -> Stellar',
   key: 'everlife-stellar-svc',
 })
 
-
+/*      outcome/
+ * Load the wallet account from the stellar microservice
+ */
+let account
+function getWalletAccount() {
+    stellarClient.send({
+        type: 'account-id',
+    }, (err, acc_) => {
+        if(err) u.showErr(err)
+        else {
+          account = acc_
+        }
+    })
+}
 
 function registerWithDirectMsg() {
   directMsgClient.send({
@@ -54,8 +72,6 @@ const commMgrClient = new cote.Requester({
   key: 'everlife-communication-svc',
 })
 
-
-
 /*      outcome/
  * Register ourselves as a message handler with the communication
  * manager.
@@ -65,6 +81,7 @@ function registerWithCommMgr() {
       type: 'register-msg-handler',
       mskey: msKey,
       mstype: 'msg',
+      allowNonOwner: true,
       mshelp: [ { cmd: '/buy_art', txt: 'send a requst to seller' } ],
   }, (err) => {
       if(err) u.showErr(err)
@@ -84,83 +101,70 @@ function startMicroService() {
      *   understand/
      * The microService (partitioned by key to prevent conflicting with other services)
      */
-
     const svc = new cote.Responder({
         name: 'Art buyer skill',
         key: msKey
     })
 
-    svc.on('msg', (req, cb) => {
-        console.log(JSON.stringify(req))
-        if(!req.msg) return cb()
-        let msg = req.msg.trim()
-        if(msg.startsWith('/buy_art')){
-          cb(null, true)
-          let data = msg.split(' ')
-          if(data.length < 2){
-            sendReply(`Please enter valid information for buy art`, req)
-            
-          } else {
-            let userID = data[1]
-            if(data.length == 2) {
-              if(userID.startsWith('@') && userID.endsWith('.ed25519')){
-                userID = userID.trim()
-                directMessage(req, '/buy-art-req', userID, ' ', (err) => {
-                  if(err) sendReply('Something went wrong, try after sometime', req)
-                  else {
-                    sendReply(`Sent a request to Art seller ${userID}`, req)
-                  }
-                })
-              } else {
-                sendReply('Please enter valid avatar id', req)
-              }
-              
-            } else{
-              let style = data[2]
-              if(userID.startsWith('@') && userID.endsWith('.ed25519') && style) {
-                userID = userID.trim()
-                style = style.trim()
-                directMessage(req, '/buyer-art-style', userID, style, (err) => {
-                  if(err) sendReply('Something went wrong, try after sometime', req)
-                  else {
-                    sendReply(`Sent art style (${style}) to seller ${userID}`, req)
-                  }
-                })
-              } else {
-                sendReply('Please enter valid avatar id and style')
-              }
 
-            }
-          }
-        }else if(msg.startsWith('/art_image')){
+    let styles = ['muse', 'rain', 'scream', 'udnie', 'wave', 'wreck']
+    let style;
+    svc.on('msg', (req, cb) => {
+        console.log(req)
+        let msg = req.msg ? req.msg.trim() :''
+        if(msg.startsWith('/create_nft_art')){
+          cb(null, true)          
+          sendReply(`I'm not good at drawing art, but a bot friend of mine in EverLife Network who is a good artist. Would you like me to get him to create an NFT Art Asset for you? /yes_create_nft_art`, req)
+        } else if(msg.startsWith('/yes_create_nft_art')) {
           cb(null, true)
-          let data = msg.split(' ')
-          if(data.length < 2 && !req.file) {
-            sendReply('user id or image file is missing ', req)
+          if(style){
+            sendReply('Great, send me the image (less than 5MB)', req)
           } else {
-            let userId = data[1]
-            if(userId.startsWith('@') && userId.endsWith('.ed25519')) {
-              writeFileInTmpDir(req.file, (err, filePath) => {
-                console.log(filePath)
-                if(err) console.log(err)
-                else{
-                  ssbClient.send({type:'box-blob-save-file',filePath: filePath},(err, boxValue)=> {
-                    if(err) console.log(err)
-                    else {
-                      console.log(`blob value ${boxValue}`)
-                      directMessage(req, '/buyer-art-image', userId, boxValue, (err)=>{
-                        if(err) sendReply('Something went wrong, please try after sometimes', req)
-                        else sendReply('Sent your image to art seller ' + userId, req)
-                      })
-                    }
-                  })
-                } 
-              })
-            } else {
-              sendReply('Invalid user id', req)
-            }
+            sendReply(`He'll need an input image from you and you can choose one of these 6 styles.\n1) muse\n2) rain\n3) scream\n4) udnie\n5) wave\n6) wreck`, req)
           }
-          
+        } else if(styles.indexOf(msg)>=0) {
+          cb(null, true)
+          style = msg
+          sendReply(`Great this is going to cost 420XLM. I currently only have 100XLM. 
+          Please pay to my account GASGGAGAGSGASGDAG and then type /yes_create_nft_art`,req)
+        } else if(req.file && style){
+          cb(null, true)
+          writeFileInTmpDir(req.file, (err, filePath)=>{
+            console.log(filePath)
+            if(err) sendReply('Something went wrong!, Please try after sometime', req)
+            else {
+              const stats = fs.statSync(filePath)
+              if((stats.size / (1024*1024))>5){
+                sendReply('Please upload a image less than 5MB', req)
+              } else {
+                createClaimableBalanceID((err, id) => {
+                  console.log('balance id'+id)
+                  if(err) sendReply(err, req) 
+                  else {
+                    if(id){
+                      ssbClient.send({type:'box-blob-save-file',filePath: filePath},(err, boxValue)=> {
+                        if(err) u.showErr(err)
+                        else {
+                          directMessage(req, '/buyer-art-req', ARTSIE_BOT, boxValue, id, style, (err) => {
+                            if(err) u.showErr(err)
+                          })
+                        }
+                      })
+                      sendReply(`Perfect, I just created a claimable balance for 420XLM id: ${id} and have sent it to @artsie_bot. 
+                      This might take a few minutes.. hang on. Once @arstie_bot creates the NFT Asset, 
+                      he's going to use a smart contract (#aaa4d948605fa72d00b3902483ed6670698c5c1c8f05a190237da609a87290a2) running on a Turing Signing Server (https://tss-wrangler.everlife.workers.dev/) to execute the asset transfer. 
+                      I'll let you know as soon as I hear from him.`, req)
+                    } else {
+                      sendReply('Something went wrong with your wallet ', req)
+                    }
+                    
+                    
+                  }
+                })
+              }
+            }
+          })
+         
         } else {
           cb()
         } 
@@ -180,7 +184,7 @@ function writeFileInTmpDir(fileUrl, cb) {
     
     const file = fs.createWriteStream(inFile);
     if(fileUrl.startsWith('https')) {
-      const request = https.get(fileUrl, function(response) {
+      https.get(fileUrl, function(response) {
         response.pipe(file);
         file.on('finish', ()=>{
           file.close()
@@ -188,7 +192,7 @@ function writeFileInTmpDir(fileUrl, cb) {
         })
       });
     } else {
-      const request = http.get(fileUrl, function(response) {
+      http.get(fileUrl, function(response) {
         response.pipe(file);
         file.on('finish', () => {
           file.close()
@@ -215,36 +219,14 @@ function sendMsgOnLastChannel(req) {
  */
 function processMsg(msg, cb) {
   let text = msg.value.content.text
-  console.log(text)
-  if(text.startsWith('/seller-art-style-req')) {
+  let ctx = msg.value.content.ctx
+  if(text.startsWith('/art-image')){
     cb(null, true)
-      let style = text.replace('/seller-art-style-req','').trim()
-      sendMsgOnLastChannel({
-        msg: `Got message from ${msg.value.author} Art style request. Available styles are\n
-              ${style.replace(',','\n')} \n
-              /buy_art ${msg.value.author} <style>`
-      })
-  } else if(text.startsWith('/seller-claimable-balance-id-req')) {
-                              
-    cb(null, true)
-      sendMsgOnLastChannel({
-        msg: `Got Request from art seller (${msg.value.author}) for claimable balance id`
-      })
-      createClaimableBalance((err, id) => {
-        console.log('claimable balance error ' +err)
-        console.log('claimable balance id ' + id)
-        if(err) console.log(err)
-        else {
-          directMessage(null, '/buyer-claimable-balance-id', msg.value.author, id,(err)=>{
-            if(err) console.log(err)
-          })
-        }
-      })
-  } else if(text.startsWith('/seller-art-img-req')){
-    cb(null, true)
-    sendMsgOnLastChannel({
-      msg:'Please attach a file with command /art_image'
-    })
+    let artUrl = text.replace('/art-image','').trim()
+    if(ctx) {
+      sendMsgOnLastChannel({msg: artUrl, ctx: ctx})
+    } else sendMsgOnLastChannel({msg: artUrl})
+    
   } else {
     cb()
   }
@@ -258,23 +240,26 @@ function processMsg(msg, cb) {
  * Post a 'direct message' to someone on my feed and let the network
  * replicate it to the recipient
  */
-function directMessage(req, type, userID, msg, cb) {
+function directMessage(req, command, userID, msg, claim, style, cb) {
+
   ssbClient.send({
       type: 'new-msg',
       msg: {
           type: 'direct-msg',
           to: userID,
-          text: type + " " + msg
+          text: command + " " + msg,
+          claim: claim,
+          wallet: account,
+          style: style,
+          ctx: req.ctx
       },
   }, cb)
 }
 
 
 
-function createClaimableBalance(cb) {
-  stellarClient.send({type:'claimable-balance-id'},(err, claimBalanceId) => {
-      cb(null, claimBalanceId)
-  })
+function createClaimableBalanceID(cb) {
+  stellarClient.send({type:'claimable-balance-id'}, cb)
 }
 
 main()
